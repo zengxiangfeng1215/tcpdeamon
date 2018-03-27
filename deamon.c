1810ee55189d1e3824b7ce72054bfc25fc2545d1
Anton <shkinder.anton@gmail.com>
2018-03-27 11:49:52 +0300

    Fixed some;add getopt()

--- deamon.c
+++ deamon.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,14 +12,7 @@
 #define STRINGSIZE 64
 #define PORTID 80
 
-char md5[] = "md5";
-char filename[] = "filename";
-char contentLength[] = "Content-Length: ";
-char prefix[] = "--------------------------";
-char boundary[] = "boundary";
-char logfilename[] = "deamon_log.txt";
-
-int log_file;
+const char logfilename[] = "deamon_log.txt";
 
 struct FileTCP {
     char md5[STRINGSIZE];
@@ -27,46 +21,34 @@ struct FileTCP {
 };
 
 int changeSwitch(char *string) {
-    if (strstr(string, md5) != NULL) {
+    if (strcasestr(string, "md5") != NULL) {
         return 1;
-    } else if (strstr(string, filename) != NULL) {
+    } else if (strcasestr(string, "filename") != NULL) {
         return 2;
     }
     return -1;
 }
 
-int checkBoundary(char *string){
-    if (strstr(string, prefix) != NULL && strstr(string, boundary) == NULL){
-        return 1;
-    }
-    return 0;
-}
-
 int checkContentLength(char *string){
-    if (strstr(string, contentLength) != NULL){
+    if (strcasestr(string, "Content-Length:") != NULL){
         return 1;
     }
     return 0;
 }
 
 int checkContentType(char *string) {
-    if (strstr(string, "Content-Type:") != NULL) {
+    if (strcasestr(string, "Content-Type:") != NULL) {
         return 1;
     }
     return 0;
 }
 
-char *pharsePost(struct FileTCP *fileTCP, char *buff, int *switcher, int *bytes_size) {
+char *parsePost(struct FileTCP *fileTCP, char *buff, int *switcher, int *bytes_size) {
     char *token = NULL;
     token = strtok(buff, "\r\n");
-    int flag = 0;
     while (token) {
         if(checkContentLength(token) == 1){
-            fileTCP->contLen = atoi(token + strlen(contentLength));
-        }
-        if(flag || checkBoundary(token)){
-            flag = 1;
-            fileTCP->contLen -= strlen(token) + 2;
+            fileTCP->contLen = atoi(token + strlen("Content-Length: "));
         }
         *bytes_size -= strlen(token) + 2;
         if (*switcher == 3 && checkContentType(token) == 1) {
@@ -92,59 +74,19 @@ char *pharsePost(struct FileTCP *fileTCP, char *buff, int *switcher, int *bytes_
     return NULL;
 }
 
-int formatPostFile(char *filename) {
-    FILE *post_file;
-    if ((post_file = fopen(filename, "rb")) != NULL) {
-        fseek(post_file, 0, SEEK_END);
-        long int size = ftell(post_file);
-        long int size_to_remove = 48;
-        long int size_to_save = size - size_to_remove;
-        fclose(post_file);
-        truncate(filename, size_to_save);
-        return 1;
-    }
-    return 0;
-}
-
-int fileMD5(char *filename, char* md5_string) {
-    unsigned char c[MD5_DIGEST_LENGTH];
-    FILE *inFile = fopen(filename, "rb");
-
-    MD5_CTX mdContext;
-    int bytes;
-    unsigned char data[SIZEBUFF];
-
-    if (inFile == NULL) {
-        perror("File can't be opened!");
-        exit(6);
-    }
-
-    MD5_Init(&mdContext);
-    while ((bytes = fread(data, 1, SIZEBUFF, inFile)) != 0)
-        MD5_Update(&mdContext, data, bytes);
-    MD5_Final(c, &mdContext);
-
-//    for (int i = 0; i < MD5_DIGEST_LENGTH; i++)
-//        printf("%02x", c[i]);
-
-    fclose(inFile);
-
-    char md5new_string[33];
-    for(int i = 0; i < MD5_DIGEST_LENGTH; ++i)
-        sprintf(&md5new_string[i*2], "%02x", c[i]);
-
-    return strcmp(md5new_string,md5_string);
-}
-
 void listenTCP() {
+    int log_file = open(logfilename, O_WRONLY | O_CREAT);
+    if(log_file < 0){
+        perror("Can't open log file!\n");
+        exit(-1);
+    }
+
     write(log_file, "Deamon started!\n", strlen("Deamon started!\n"));
 
     int sock;
     struct sockaddr_in addr;
     int bytes_read;
     char *string_buff[SIZEBUFF];
-    struct FileTCP fileTCP;
-    fileTCP.contLen = 0;
 
     int listener = socket(AF_INET, SOCK_STREAM, 0);
     if (listener < 0) {
@@ -172,6 +114,13 @@ void listenTCP() {
             exit(3);
         }
         FILE *post_file;
+        struct FileTCP fileTCP;
+        fileTCP.contLen = 0;
+
+        //md5
+        unsigned char c[MD5_DIGEST_LENGTH];
+        MD5_CTX mdContext;
+        MD5_Init(&mdContext);
 
         int switcher = -1;
         char *pointer = NULL;
@@ -182,9 +131,14 @@ void listenTCP() {
                 break;
             if (pointer == NULL) {
                 if(fileTCP.contLen != 0){
-                    fileTCP.contLen -= bytes_read;
+                    if(fileTCP.contLen == bytes_read){
+                        fileTCP.contLen -= bytes_read;
+                        bytes_read -= 48;
+                    }else {
+                        fileTCP.contLen -= bytes_read;
+                    }
                 }
-                pointer = pharsePost(&fileTCP, (char *) string_buff, &switcher, &bytes_read);
+                pointer = parsePost(&fileTCP, (char *) string_buff, &switcher, &bytes_read);
                 post_file = fopen((char *) fileTCP.filename, "wb");
                 if (pointer != NULL) {
                     if(post_file == NULL){
@@ -192,72 +146,90 @@ void listenTCP() {
                         break;
                     }
                     fwrite(pointer, sizeof(char), bytes_read - 6, post_file);
+                    MD5_Update(&mdContext, pointer, bytes_read - 6);
                 }
             } else {
                 fileTCP.contLen -= bytes_read;
-                fwrite(string_buff, sizeof(char), bytes_read, post_file);
+                if(fileTCP.contLen <= 48){
+                    if(bytes_read >= 48){
+                        fwrite(string_buff, sizeof(char), bytes_read - (48 - fileTCP.contLen), post_file);
+                        MD5_Update(&mdContext, string_buff, bytes_read - (48 - fileTCP.contLen));
+                    }
+                }else {
+                    fwrite(string_buff, sizeof(char), bytes_read, post_file);
+                    MD5_Update(&mdContext, string_buff, bytes_read);
+                }
             }
             bzero(string_buff, SIZEBUFF);
-            if(fileTCP.contLen <= 0){
+            if(fileTCP.contLen <= 48){//if(fileTCP.contLen <= 0)
                 shutdown(sock, 1);
             }
         }
-
         close(sock);
         fclose(post_file);
+        MD5_Final(c, &mdContext);
+        char md5new_string[33];
+        for(int i = 0; i < MD5_DIGEST_LENGTH; ++i)
+            sprintf(&md5new_string[i*2], "%02x", c[i]);
 
-        if (formatPostFile((char *) fileTCP.filename) == 0) {
-            write(log_file, "truncate Error!\n", strlen("truncate Error!\n"));
-            exit(5);
-        }
 
-        if (fileMD5((char *) fileTCP.filename, (char *) fileTCP.md5) != 0) {
+        if (strcmp(md5new_string, fileTCP.md5) != 0) {
             remove((char *) fileTCP.filename);
-            write(log_file, "Hash md5 failed! File not saved!\n", strlen("Hash md5 failed! File not saved!\n"));
+            write(log_file, "Hash md5 failed! File not saved! : ", strlen("Hash md5 failed! File not saved! : "));
+            write(log_file, fileTCP.filename, strlen(fileTCP.filename));
+            write(log_file, "\nReceived file md5 : ", strlen("\nReceived file md5 : "));
+            write(log_file, md5new_string, strlen(md5new_string));
+            write(log_file, "\n", strlen("\n"));
         }else{
-            write(log_file, "File received and saved!\n", strlen("File received and saved!\n"));
+            write(log_file, "File received and saved! : ", strlen("File received and saved! : "));
+            write(log_file, fileTCP.filename, strlen(fileTCP.filename));
+            write(log_file, "\n", strlen("\n"));
         }
     }
     close(listener);
 }
 
+void printUsage(){
+    printf("Usage:\n\t-d\tfor daemon\n\t-i\tfor interactive\n\t-?\n\t-h\tfor help\n");
+}
+void printHelp(){
+    printf("TCP LINUX Deamon\nListens to a TCP port and accepts files via HTTP/POST\n");
+    printf("It also checks the md5 sum of the file.\nIf it does not match the one in the parameters - the file will not saved.\n\n");
+    printUsage();
+    printf("\n\nAuthor: Anton Shkinder\nEmail: shkinder.anton@gmail.com\n\n");
+}
 
 int main(int argc, char **argv) {
     pid_t parpid;
 
-    if (argc < 2)
-    {
-        printf("Usage: ./daemon -d for daemon or ./daemon -i for interactive \n Also send WD in 3 param if you need.\n");
-        exit(1);
+    const char optString[] = "dih?";
+    int opt = getopt(argc, argv, optString);
+    if(opt == -1){
+        printUsage();
     }
-
-    if (argc == 3){
-        chdir(argv[2]);
-    }
-
-    log_file = open(logfilename, O_WRONLY | O_CREAT);
-    if(log_file < 0){
-        perror("Can't open log file!\n");
-    }
-
-    if (strcmp(argv[1],"-i")==0)
-        listenTCP();
-    else if (strcmp(argv[1],"-d")==0)
-    {
-        if((parpid=fork())<0)
-        {
-            printf("\nCan't fork");
-            exit(1);
+    while(opt != -1){
+        switch (opt){
+            case 'd':
+                if((parpid=fork())<0)
+                {
+                    printf("\nCan't fork");
+                    exit(1);
+                }
+                else if (parpid!=0)
+                    exit(0);
+                setsid();
+                listenTCP();
+                break;
+            case 'i':
+                listenTCP();
+            case -1:
+            case 'h':
+            case '?':
+            default:
+                printHelp();
+                break;
         }
-        else if (parpid!=0)
-            exit(0);
-        setsid();
-        listenTCP();
-    }
-    else
-    {
-        printf("Usage: ./daemon -d for daemon or ./daemon -i for interactive\n Also send WD in 3 param if you need.\n");
-        exit(1);
+        opt = getopt(argc, argv, optString);
     }
     return 0;
 }
